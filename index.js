f3616ef3106216684d6df3626847b4420e3b9b85
shubhamurkude79 <shubhamurkude79@gmail.com>
2024-05-07 01:27:35 +0530

    done mongoose connection and bodyparser setup and created schema in crmModel.js

--- index.js
+++ index.js
@@ -1,8 +1,20 @@
 import express from 'express';
 import routes from './src/routes/crmRoutes';
+import mongoose from 'mongoose';
+import bodyParser from 'body-parser';
 
 const app = express();
 const PORT = 3000;
+
+//mongoose connection
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost/CRMdb',{
+    useNewUrlParser: true
+})
+
+//body parser setup
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 routes(app);
 
